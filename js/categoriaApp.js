98bc5f9a29ecc9d0e55debc746c6d0990fc93335
Mateo Lohezic <mateolohezic@gmail.com>
2022-11-09 11:16:34 -0300

    numero barra lateral

--- js/categoriaApp.js
+++ js/categoriaApp.js
@@ -53,5 +53,58 @@ getCategoriaActual()
 
 const setCategoria = async (numeroCat) => {
     localStorage.setItem("categoria", numeroCat)
-    window.location.href= "./categoria.html"
+    window.location.href= "../categoria.html"
 }
+
+
+const getProductosTotal = async () => {
+    const resultado = await fetch('http://localhost:3000/productos/');
+    const resultados = await resultado.json();
+    const resultadoo = resultados.filter(producto => producto.publicado == true )
+    return resultadoo;
+}
+
+const imprimirNumerosCategorias = async () =>{
+    const productosTotales = await getProductosTotal()
+    const numero1 = document.getElementById(`numeroCategoria1`)
+    const numero2 = document.getElementById(`numeroCategoria2`)
+    const numero3 = document.getElementById(`numeroCategoria3`)
+    const numero4 = document.getElementById(`numeroCategoria4`)
+    const numero5 = document.getElementById(`numeroCategoria5`)
+    const numero6 = document.getElementById(`numeroCategoria6`)
+    const numero7 = document.getElementById(`numeroCategoria7`)
+    const numero8 = document.getElementById(`numeroCategoria8`)
+    const numero9 = document.getElementById(`numeroCategoria9`)
+    const numero10 = document.getElementById(`numeroCategoria10`)
+    const numero11 = document.getElementById(`numeroCategoria11`)
+    const numero12 = document.getElementById(`numeroCategoria12`)
+
+    const pcNot = productosTotales.filter(producto => producto.categoria == "PC" || producto.categoria == "Notebook")
+    const consolas = productosTotales.filter(producto => producto.categoria == "Consola")
+    const procCoolers = productosTotales.filter(producto => producto.categoria == "Procesador" || producto.categoria == "Refrigeración")
+    const mother = productosTotales.filter(producto => producto.categoria == "Motherboard")
+    const ram = productosTotales.filter(producto => producto.categoria == "RAM")
+    const almacenamiento = productosTotales.filter(producto => producto.categoria == "Almacenamiento")
+    const gpu = productosTotales.filter(producto => producto.categoria == "GPU")
+    const gabFuentes = productosTotales.filter(producto => producto.categoria == "Gabinete" || producto.categoria == "Fuente")
+    const tecladoMouse = productosTotales.filter(producto => producto.categoria == "Teclado" || producto.categoria == "Mouse")
+    const monitores = productosTotales.filter(producto => producto.categoria == "Pantalla")
+    const auriculares = productosTotales.filter(producto => producto.categoria == "Auriculares")
+    const otros = productosTotales.filter(producto => producto.categoria != "Auriculares" && producto.categoria != "PC" && producto.categoria != "Notebook" && producto.categoria != "Consola" && producto.categoria != "Procesador" && producto.categoria != "Refrigeración" && producto.categoria != "Motherboard" && producto.categoria != "RAM" && producto.categoria != "Almacenamiento" && producto.categoria != "GPU" && producto.categoria != "Gabinete" && producto.categoria != "Fuente" && producto.categoria != "Teclado" && producto.categoria != "Mouse" && producto.categoria != "Pantalla" && producto.categoria != "Auriculares")
+
+    
+    numero1.innerHTML = `(${pcNot.length})`
+    numero2.innerHTML = `(${consolas.length})`
+    numero3.innerHTML = `(${procCoolers.length})`
+    numero4.innerHTML = `(${mother.length})`
+    numero5.innerHTML = `(${ram.length})`
+    numero6.innerHTML = `(${almacenamiento.length})`
+    numero7.innerHTML = `(${gpu.length})`
+    numero8.innerHTML = `(${gabFuentes.length})`
+    numero9.innerHTML = `(${monitores.length})`
+    numero10.innerHTML = `(${tecladoMouse.length})`
+    numero11.innerHTML = `(${auriculares.length})`
+    numero12.innerHTML = `(${otros.length})`
+}
+
+imprimirNumerosCategorias()
